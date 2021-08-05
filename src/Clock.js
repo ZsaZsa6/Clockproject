84469a456fcaec0e12f3dedf333ecae051523104
Ronda Cowen <cowenhome5@gmail.com>
2021-08-05 16:20:43 -0500

    set up components and clear unneccessary code

---
+++ src/Clock.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Clock() {
+	return (
+		<div>
+			<div className="clock">
+				<div className="clock-hands">
+					<div className="hour-hand"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
